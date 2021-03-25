bd1105d9cc1a129c2b21800a744d1e62caa6e0f0
Latifa Ghassan Abdul-Halim Amaireh <lalooshamayra@hotmail.com>
2021-03-25 16:06:03 +0200

    fix some dynamic components

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,17 +1,17 @@
-prompt('What is you name?');
 var name1 = prompt('What is your name?');
-alert('Welcome'+name1);
+alert('Welcome '+ name1);
 
-var covid=prompt('Did you take the medicine?','YES,NO');
+var Answer= prompt('Did you take the Covid-19 vaccine?','YES,NO');
 
-if(covid ==='YES'){
-document.write('<img src="covid.jpg" height="200px" width="200px">');
+if(Answer ==='yes'){
+document.write('<img src="covid.jpg">');
 }
-else if(covid === 'NO') {
-document.write('<img src="https://c.files.bbci.co.uk/3A97/production/_116299941_oxford_coronavirus_vaccine_640_3x-nc.png" height="200px" width="200px">');
+else if(Answer === 'no') {
+document.write('<img src="https://c.files.bbci.co.uk/3A97/production/_116299941_oxford_coronavirus_vaccine_640_3x-nc.png" height="500px" width="500px">');
 
 }
-else (covid-19 === 'Please choose again!');
+else{
+    console.log('anything')
 }
 
 
