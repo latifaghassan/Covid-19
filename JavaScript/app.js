dab1ee22a3d3d586ebea5f4fb74e628e40da9c71
Latifa Ghassan Abdul-Halim Amaireh <lalooshamayra@hotmail.com>
2021-03-25 15:40:54 +0200

    added some JV stuff

---
+++ JavaScript/app.js
@@ -0,0 +1,18 @@
+prompt('What is you name?');
+var name1 = prompt('What is your name?');
+alert('Welcome'+name1);
+
+var covid=prompt('Did you take the medicine?','YES,NO');
+
+if(covid ==='YES'){
+document.write('<img src="covid.jpg" height="200px" width="200px">');
+}
+else if(covid === 'NO') {
+document.write('<img src="https://c.files.bbci.co.uk/3A97/production/_116299941_oxford_coronavirus_vaccine_640_3x-nc.png" height="200px" width="200px">');
+
+}
+else (covid-19 === 'Please choose again!');
+}
+
+
+
